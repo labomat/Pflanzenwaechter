26bf9354c5323170f8e318c3a35b045b23bf4e6c
labomat <kai07@laborenz.de>
2018-07-15 17:22:03 +0200

    added rtc memory for last value
    last measured value is stored in rtc memory to survive deep sleep

--- Feuchtesensor_IoT.ino
+++ Feuchtesensor_IoT.ino
@@ -1,5 +1,5 @@
 // 
-// IoT Moisture sensor v1
+// IoT Moisture sensor v2
 //
 // based on
 // Adafruit IO Analog In Example
@@ -24,7 +24,6 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 
-
 // Adafruit IO configuration
 #include "config.h"
 
@@ -39,14 +38,24 @@ extern "C" {
 // set up the 'moisture' feed on Adafruit IO (must be configured there as well)
 AdafruitIO_Feed *moisture = io.feed("moisture");
 
+
 // io port to power the moisture sensor on demand
 #define SENSOR_POWER_PIN D8
 
 // data state
-int current = 0;
+int currentVal = 0;
 int median = 0;
 int samples = 5;
 int last = -1;
+
+// data structure for storing data in rtc memoy during deep sleep
+// see https://github.com/SensorsIot/ESP8266-RTC-Memory 
+typedef struct {
+  int last;
+  int counter;
+} rtcStore;
+
+rtcStore rtcMem;
 
 // sleep for this many seconds
 const int sleepSeconds = 600;
@@ -91,6 +100,14 @@ void setup() {
   // io.adafruit.com, and processes any incoming data.
   io.run();
 
+  // read stored value from rtc memory
+  // parameter: address (starts at 64), variable name, size in byte (4 byte steps)
+  system_rtc_mem_read(64, &rtcMem, sizeof(rtcMem));
+  Serial.print("Counter: ");
+  Serial.println(rtcMem.counter);
+  Serial.print("Last reading -> ");
+  Serial.println(rtcMem.last);
+
   // enabling sensor power
   digitalWrite(SENSOR_POWER_PIN,HIGH);
   delay(200);
@@ -100,16 +117,16 @@ void setup() {
   // taking several readings and calculate median
 
   for (int i = 0; i < samples; i++) {
-    //current = analogRead(MOISTURE_PIN);
-    current = map(analogRead(MOISTURE_PIN), 400, 900, 100, 0);
+    //currentVal = analogRead(MOISTURE_PIN);
+    currentVal = map(analogRead(MOISTURE_PIN), 400, 900, 100, 0);
     Serial.print(i);
     Serial.print(": ");
-    Serial.print(current);
-    median = median + current;
-  
+    Serial.print(currentVal);
+    median = median + currentVal;
+    
     Serial.print(" | ");
     Serial.println(median);
-    delay(200);
+    delay(100);
   }
 
   //shutting down sensor power
@@ -119,22 +136,36 @@ void setup() {
   Serial.print("Median: ");
   Serial.println(median);
 
-  // save the current state to the moisture feed to Adafruit IO
-  digitalWrite(BUILTIN_LED,LOW);
-  Serial.print("sending new data to io -> ");
-  Serial.println(median);
-  moisture->save(median);
-  delay(500);
-  digitalWrite(BUILTIN_LED,HIGH);
-  Serial.printf("Going to sleep for %d seconds\n\n", sleepSeconds);
-   
+  // do nothing if the value hasn't changed since last measurement
+  if(median == rtcMem.last) {
+    // set esp to sleep
+    Serial.printf("Nothing changed - going to sleep for %d seconds\n\n", sleepSeconds);
+    }
+  else {
+    // save the current state to the moisture feed to Adafruit IO
+    digitalWrite(BUILTIN_LED,LOW);
+    Serial.print("sending new data to io -> ");
+    Serial.println(median);
+    moisture->save(median);
+    delay(500);
+    digitalWrite(BUILTIN_LED,HIGH);
+    Serial.printf("Going to sleep for %d seconds\n\n", sleepSeconds);
+    }
+  
+  // store last state and counter in rtc memory
+  rtcMem.last = median;
+  rtcMem.counter = rtcMem.counter + 1;
+  // rtcMem.counter = 0;
+  if (rtcMem.counter < 0) {
+    rtcMem.counter = 0;
+  }
+  system_rtc_mem_write(64, &rtcMem, sizeof(rtcMem));
+  
   // set esp to sleep
   ESP.deepSleep(sleepSeconds * 1000000);
   
 }
 
 void loop() {
-
   
 }
-
